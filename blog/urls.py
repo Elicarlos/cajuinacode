12d342661ef5651c0195b8c38edca658881b9bdb
Elicarlos <elicarlosantos_@hotmail.com>
2022-09-02 03:31:17 -0300

    iniciando projeto

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from pkgutil import ImpImporter
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog')
+]
